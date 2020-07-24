20f168ddfde49b13aeaf10f7ae7ef869d9c90e18
hiitoos <adrianhitos@gmail.com>
2020-07-24 22:04:49 +0200

    Class Zapatillas modified, setters on constructor and new empty constructor too

--- src/main/java/Zapatillas.java
+++ src/main/java/Zapatillas.java
@@ -9,11 +9,63 @@ public class Zapatillas {
     float precio;
 
     public Zapatillas(long id_zapatillas, String genero, String modelo, String color, float talla, float precio) {
+        setId_zapatillas(id_zapatillas);
+        setGenero(genero);
+        setModelo(modelo);
+        setColor(color);
+        setTalla(talla);
+        setPrecio(precio);
+    }
+
+    public Zapatillas(){
+
+    }
+
+    public long getId_zapatillas() {
+        return id_zapatillas;
+    }
+
+    public void setId_zapatillas(long id_zapatillas) {
         this.id_zapatillas = id_zapatillas;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
         this.genero = genero;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
         this.modelo = modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
         this.color = color;
+    }
+
+    public float getTalla() {
+        return talla;
+    }
+
+    public void setTalla(float talla) {
         this.talla = talla;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
         this.precio = precio;
     }
 
