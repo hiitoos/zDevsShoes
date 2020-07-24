45aa17ed85edcf430b3ee1c9e30c207a55097df4
hiitoos <adrianhitos@gmail.com>
2020-07-24 15:09:02 +0200

    Client class added

--- src/main/java/Cliente.java
+++ src/main/java/Cliente.java
@@ -1,3 +1,72 @@
-public class Cliente {
+import java.util.Date;
 
+public class Cliente {
+    int id_Clientes;
+    String nombre;
+    String apellido1;
+    String apellido2;
+    String email;
+    int telefono;
+    Date fecha_nacimiento;
+
+    public Cliente(String nombre, String apellido1, String apellido2, String email, int telefono, Date fecha_nacimiento) {
+        this.nombre = nombre;
+        this.apellido1 = apellido1;
+        this.apellido2 = apellido2;
+        this.email = email;
+        this.telefono = telefono;
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+
+    public int getId_Clientes() {
+        return id_Clientes;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido1() {
+        return apellido1;
+    }
+
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+    public String getApellido2() {
+        return apellido2;
+    }
+
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public Date getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+
+    public void setFecha_nacimiento(Date fecha_nacimiento) {
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
 }
