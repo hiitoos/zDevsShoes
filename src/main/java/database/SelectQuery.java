e57bed1bbfb7d45c0fddf8fdeb5fb1978af3685b
hiitoos <adrianhitos@gmail.com>
2020-07-27 14:45:41 +0200

    Solved merge problem

--- src/main/java/database/SelectQuery.java
+++ src/main/java/database/SelectQuery.java
@@ -1,24 +1,40 @@
 package database;
 
+import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class SelectQuery {
-    public SelectQuery(Statement stmt, String var){
+    static ConnectionDB dataBase;
+    static Connection conn;
+    static java.sql.Statement stmt;
+
+    public SelectQuery () throws SQLException {
+        dataBase = new ConnectionDB();
+        conn = dataBase.getConnection();
+        stmt = conn.createStatement();
+    }
+
+    public void closeConn() throws SQLException {
+        stmt.close();
+        conn.close();
+    }
+
+    public void launchQuery(String var){
         String SQL;
         ResultSet rs;
         try {
             if (var.equals("total")) {
                 SQL = "SELECT COUNT(*) FROM pedidos WHERE pagado = 1";
-               rs = stmt.executeQuery(SQL);
+                rs = stmt.executeQuery(SQL);
                 while (rs.next()) {
                     int count = rs.getInt("count(*)");
                     System.out.println("La cantidad total de ventas es de " + count);
                 }
             }
 
-            if (var.equals("pais")){
+            else if (var.equals("pais")){
                 SQL ="SELECT paises.id_pais, paises.nombre, COUNT(pedidos.id_pedidos) as total FROM direcciones LEFT JOIN clientes ON direcciones.id_cliente=clientes.id_Clientes LEFT JOIN paises ON direcciones.id_pais = paises.id_pais LEFT JOIN pedidos ON pedidos.id_cliente = clientes.id_Clientes WHERE pedidos.pagado = 1 GROUP BY paises.id_pais";
                 rs = stmt.executeQuery(SQL);
                 while (rs.next()) {
@@ -28,7 +44,7 @@ public class SelectQuery {
                 }
             }
 
-            if (var.equals("usuario")){
+            else if (var.equals("usuario")){
                 SQL = "SELECT COUNT(*) FROM clientes";
                 rs = stmt.executeQuery(SQL);
                 while (rs.next()) {
@@ -37,7 +53,7 @@ public class SelectQuery {
                 }
             }
 
-            if (var.equals("nofinalizados")) {
+            else if (var.equals("nofinalizados")) {
                 SQL = "SELECT DISTINCT nombre, email FROM clientes INNER JOIN pedidos ON (clientes.id_clientes = pedidos.id_cliente) WHERE pedidos.pagado = 0";
                 rs = stmt.executeQuery(SQL);
                 while (rs.next()) {
@@ -47,7 +63,7 @@ public class SelectQuery {
                 }
             }
 
-            if (var.equals("mascompras")){
+            else if (var.equals("mascompras")){
                 SQL = "SELECT nombre, apellido1, apellido2, COUNT(*) FROM clientes LEFT JOIN pedidos ON (clientes.id_Clientes = pedidos.id_cliente) WHERE pedidos.pagado = 1 GROUP BY id_cliente ORDER BY COUNT(*)";
                 rs = stmt.executeQuery(SQL);
                 while (rs.next()) {
@@ -58,7 +74,7 @@ public class SelectQuery {
             }
 
         }catch (SQLException exception) {
-            System.out.println("Error al rollback and save point" + exception);
+            System.out.println("Error: " + exception);
         }
     }
 }
