fda24e43755fab0c030f23fb8041087f73ef9000
hiitoos <adrianhitos@gmail.com>
2020-07-27 08:49:22 +0200

    updated main, added close.conn

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,6 +14,7 @@ public class Main {
     public static void main(String[] args) throws SQLException {
         sq = new SelectQuery();
         menu();
+        sq.closeConn();
     }
 
     public static void menu(){
